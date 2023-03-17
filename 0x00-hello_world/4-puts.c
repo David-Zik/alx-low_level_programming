a890f512e719db6b3d3abdbe91da08426fb03409
David-Zik <davidezeike6@gmail.com>
2023-03-17 00:04:35 +0000

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints "Programming is like building a multilingual puzzle followed by a new line.
+ * Return: Always 0..
+ */
+
+int main(void)
+
+{
+	put("\"Programming is line building a multilingual puzzle\n");
+
+	return(0);
+}

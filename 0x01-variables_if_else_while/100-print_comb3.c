7d845a3b7934341c6ac007b3c2762932c4d29451
David <davidezeike6@gmail.com>
2023-03-18 12:40:26 -0700

    Prints all different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,22 +12,20 @@ int main(void)
 {
 	int a, b;
 
-	for (a = 0; a < 9; a++)
+	for (a = '0'; a < '9'; a++)
 	{
-		for (b = a + 1; b <= 9; b++)
-		{
-			if (b != a)
-			{
-				putchar('0' + a);
-				putchar('0' + b);
-			}
-			if (a == '8' && b == '9')
-			continue;
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
+	for (b = a + 1; b <= '9'; b++)
+	{
+	if (b != a)
+	{
+	putchar(a);
+	putchar(b);
+	if (a == '8' && b == '9')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
 	}
 	putchar('\n');
 	return (0);

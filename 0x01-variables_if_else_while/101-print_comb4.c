5f1efce7d0fa6f9a28914053487bf3c8bec16091
David <davidezeike6@gmail.com>
2023-03-18 22:47:47 -0700

    Prints different combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,11 +12,11 @@ int main(void)
 {
 	int a, b, c;
 
-	for (a = '0'; a < '9'; a++)
+	for (a = '0'; a < '8'; a++)
 	{
-	for (b = a + 1; b <= '9'; b++)
+	for (b = a + 1; b < '9'; b++)
 	{
-	for (c = b + 1; b <= '9'; c++)
+	for (c = b + 1; c <= '9'; c++)
 	{
 	if ((b != a) != c)
 	{

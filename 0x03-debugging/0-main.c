b17b6463c5e2395bfef576176c2f3e6a4059902b
David-Zik <davidezeike6@gmail.com>
2023-03-22 17:45:48 +0000

    update

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Test the function positive_or_negative() gives the correct output
+ * when given a case of 0.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(0)
+
+	return (0)
+}

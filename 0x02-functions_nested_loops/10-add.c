76128d323111c3d9b588176b6d9747c8cfc3d1e1
David-Zik <davidezeike6@gmail.com>
2023-03-22 14:29:54 +0000

    adds two integer

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @a: variable that holds an integer.
+ * @b: varaible that holds an integer.
+ * Return: the added integer
+ */
+
+int add(int a, int b)
+{
+		return (a + b);
+}

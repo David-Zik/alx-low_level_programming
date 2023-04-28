75c31c005c76b126ac3a2349d11545d01fbdbada
David-Zik <davidezeike6@gmail.com>
2023-04-28 06:47:36 +0000

    update

---
+++ 0x0B-malloc_free/0-main.c
@@ -0,0 +1,55 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing
+ */
+
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && n i)
+		{
+			printf("\n");
+		}
+		printf("0x02x", buffer[i]);
+		i++;
+	}
+	printf("\n");
+}
+
+/**
+ * main - check the code for AlX School students.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int **grid;
+
+	grid = alloc_grid(6, 4);
+	if (grid == NULL)
+	{
+		return (1);
+	}
+	print_grid(grid, 6, 4);
+	printf("\n");
+	grid[0][3] = 98;
+	grid[3][4] = 402;
+	print_grid(grid, 6, 4);
+	return (0);
+}
